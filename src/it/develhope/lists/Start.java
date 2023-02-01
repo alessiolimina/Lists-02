6c44563e282f96ead436c0aff0636f8445c6c7f6
Alessio Limina <alessio.limina@gmail.com>
2023-02-01 19:56:05 +0100

    Exercise: lists 2

--- src/it/develhope/lists/Start.java
+++ src/it/develhope/lists/Start.java
@@ -26,39 +26,12 @@ public class Start {
        //FIXME write again the line above using an iterator
        System.out.println("This is random numbers' list without duplicates: " + randomNumbers);
        System.out.println("This new list has " + randomNumbers.size() + " items");
-       Collections.sort(randomNumbers);
-       //TODO complete these task using Collections.sort() in descending order,
-       // using Collections.reverseOrder() as a comparator for the Collections.sort() method
-       // understand what's a comparator and how it works
-       System.out.println(randomNumbers);
+       Collections.sort(randomNumbers, Collections.reverseOrder());
+       System.out.println("The random numbers in reverse order are: " + randomNumbers);
 
 
        System.out.println("------------------------------------------");
 
 
+         }
     }
-    }
-
-
-
-/*
-define a testing class with a main() method where you:
-define a List of integers called divisorsOf52 that contains all the divisors of 52
-define a List of integers called randomNumbers that contains the following numbers: 1, 60, 13, 12
-use a specific method of List for appending all the elements of divisorsOf52 at the bottom of randomNumbers
-print the elements of randomNumbers and the number of items inside randomNumbers
-find the positions of the duplicates and remove them, leaving just 1 occurrence of a specific integer
-print again the elements of randomNumbers and the number of items inside randomNumbers
-sort randomNumbers in descending order using Collections.sort()
-print again the elements of randomNumbers
- */
-
-
-
-// use the .addAll() method to append all the List at the bottom
-
-
-// find the positions of the duplicates and remove the first occurrence
-
-
-// use Collections.reverseOrder() as a comparator for the Collections.sort() method
