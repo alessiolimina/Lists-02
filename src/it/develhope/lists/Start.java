981859e0eb242cf0794307b78b164ffac7540e51
Alessio Limina <alessio.limina@gmail.com>
2023-02-01 19:42:54 +0100

    Exercise: lists 2

--- src/it/develhope/lists/Start.java
+++ src/it/develhope/lists/Start.java
@@ -1,41 +1,46 @@
 package it.develhope.lists;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Start {
     public static void main(String[] args) {
-       Integer[] s1 = new Integer[]{1,2,4,13,26,52};
-       List<Integer> divisorsOf52 = Arrays.asList(s1);
-       System.out.println(divisorsOf52);
 
-       List<Integer> randomNumbers= new ArrayList<>();
+       System.out.println("---------------Starting------------------");
+
+       Integer[] s1 = new Integer[]{1, 2, 4, 13, 26, 52};
+       List<Integer> divisorsOf52 = Arrays.asList(s1);
+       System.out.println("These are the divisors of 52: " + divisorsOf52);
+
+       List<Integer> randomNumbers = new ArrayList<>();
        randomNumbers.add(1);
        randomNumbers.add(60);
        randomNumbers.add(13);
        randomNumbers.add(12);
-       System.out.println(randomNumbers);
+       System.out.println("This is the random numbers' list: " + randomNumbers);
        randomNumbers.addAll(divisorsOf52);
+       System.out.println("This is the updated random numbers' list: " + randomNumbers);
+       System.out.println("This list has " + randomNumbers.size() + " items");
+
+       randomNumbers = randomNumbers.stream().distinct().collect(Collectors.toList());
+       //FIXME write again the line above using an iterator
+       System.out.println("This is random numbers' list without duplicates: " + randomNumbers);
+       System.out.println("This new list has " + randomNumbers.size() + " items");
+       Collections.sort(randomNumbers);
+       //TODO complete these task using Collections.sort() in descending order,
+       // using Collections.reverseOrder() as a comparator for the Collections.sort() method
+       // understand what's a comparator and how it works
        System.out.println(randomNumbers);
-       System.out.println(randomNumbers.size());
-       Iterator<Integer> i = randomNumbers.iterator();
-       while (i.hasNext()) {
-          Integer i1 = i.next();
-          if(i1.equals(randomNumbers.get(i.next()))){
-             randomNumbers.remove(i1);
-          }
-
-       }
-       System.out.println(randomNumbers);
-       System.out.println(randomNumbers.size());
 
 
+       System.out.println("------------------------------------------");
 
 
     }
-}
+    }
+
+
+
 /*
 define a testing class with a main() method where you:
 define a List of integers called divisorsOf52 that contains all the divisors of 52
